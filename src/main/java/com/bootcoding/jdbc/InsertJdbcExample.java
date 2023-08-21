3e44b4bb27e8abae532ea271a0d7e2eaf2311b1d
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-08-21 23:10:33 +0530

    Create InsertJdbcExample.java

---
+++ src/main/java/com/bootcoding/jdbc/InsertJdbcExample.java
@@ -0,0 +1,31 @@
+package com.bootcoding.jdbc;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class InsertJdbcExample {
+    public static void main(String[] arg){
+        try{
+            // 1. Load JDBC Driver
+            Class.forName("org.postgresql.Driver");
+
+            // 2. Get Database Connection Object
+            Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/bootcoding",
+                    "postgres", "Pranay@123");
+
+            // 3. Query Database using Statement Object
+            Statement stmt = conn.createStatement();
+            String sql = "INSERT INTO customer VALUES(2000, 'Raju', 'Nagpur', 'raju@nagpur.com', 123)";
+            // 4. Execute Query and Get ResultSet
+            stmt.executeUpdate(sql);
+            // 4.1 Extract Result
+
+
+            // 5.Connection CLose;
+            conn.close();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+}
