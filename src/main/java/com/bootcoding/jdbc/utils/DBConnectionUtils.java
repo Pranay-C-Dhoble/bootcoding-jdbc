8f3fef94983b60ac9e91c27c88242ebe8f907151
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-08-23 00:54:55 +0530

    Create DBConnectionUtils.java

---
+++ src/main/java/com/bootcoding/jdbc/utils/DBConnectionUtils.java
@@ -0,0 +1,20 @@
+package com.bootcoding.jdbc.utils;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnectionUtils {
+
+    public static Connection getConnection(){
+        try{
+            // 1. Load JDBC Driver
+            Class.forName("org.postgresql.Driver");
+
+            // 2. Get Database Connection Object
+            Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/bootcoding","postgres", "Pranay@123");
+            return conn;
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return null;
+    }
+}
