db79074ba9f387c607852fa5384d52c34c8cf99a
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-08-22 22:58:37 +0530

    Update SelectJdbcExample.java

--- src/main/java/com/bootcoding/jdbc/SelectJdbcExample.java
+++ src/main/java/com/bootcoding/jdbc/SelectJdbcExample.java
@@ -16,9 +16,36 @@ public class SelectJDBCExample {
 
             // 3. Query Database using Statement Object
             Statement stmt = conn.createStatement();
+            //purpose of Statement object is to execute queries against the database.
+            //Random access to the result set is not possible with a Statement object.
+
+            //there are three types of statement object
+            //1. Statement
+            //2. PreparedStatement
+            //3. CallableStatement
+            //Statement is used to execute static SQL queries and returns the result it produces.
+
+            //prepareStatement() method of Connection interface is used to return the PreparedStatement object.
+            //The PreparedStatement is a slightly more powerful version of a Statement,
+            // and should always be at least as quick and easy to use as a Statement.
+            //insert, update, delete, select, and so on.
+            //insert, update, delete are used to perform DML operations.
+
+
+            //Callable Statement is used to access the database stored procedures.
+            //CallableStatement is a subinterface of PreparedStatement.
+            //CallableStatement can be used when you want to access database stored procedures.
+
 
             // 4. Execute Query and Get ResultSet
             ResultSet rs = stmt.executeQuery("SELECT * FROM customer");
+            //******ResultSet is a Java object that is used to store the data returned by a query.
+            //******ResultSetMetaData is an interface that is used to get information about the columns of a ResultSet.
+
+            //******ResultSetMetaData metadata = rs.getMetaData();
+            //******returns the number of columns in the ResultSet object.
+
+
             // 4.1 Extract Result
 
             while(rs.next()){
@@ -58,4 +85,5 @@ public class SelectJDBCExample {
 //The try block contains the code that might throw an exception and the catch block handles those exceptions.
 
 //handle means to deal with or control a situation or problem
+
 
