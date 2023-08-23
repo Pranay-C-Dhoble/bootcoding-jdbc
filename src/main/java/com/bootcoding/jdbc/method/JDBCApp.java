95e44064c37ad6061fc08ef2547faabd4fcc0982
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-08-23 13:54:49 +0530

    Update JDBCApp.java

--- src/main/java/com/bootcoding/jdbc/method/JDBCApp.java
+++ src/main/java/com/bootcoding/jdbc/method/JDBCApp.java
@@ -5,9 +5,13 @@ import java.util.Map;
 
 public class JDBCApp {
     public static void main(String[] args) {
-        JdbcUsingMethod jdbcUsingMethod = new JdbcUsingMethod();
+        JdbcUsingMethod jdbcUsingMethod = new JdbcUsingMethod();//creating object of JdbcUsingMethod class
         // Get All Customers
         System.out.println("Get All Customers");
+
+        //Syntax of creating List
+        //List<ClassName> listName = new ArrayList<>();
+
         List<Customer> customers = jdbcUsingMethod.getAllCustomers();
         System.out.println("Customer Id | Customer Name | City | Email | Salesman Id");
         for(Customer customer : customers){
